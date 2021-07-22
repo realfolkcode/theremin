d472c5f835ad56d51f94a442da83603cadfd85a2
Dmitry Gavrilyev <64730991+realfolkcode@users.noreply.github.com>
2021-07-23 03:47:42 +0900

    Add DDSP model loading

--- __main__.py
+++ __main__.py
@@ -3,6 +3,18 @@ import theremin
 import cv2
 import time
 import sounddevice as sd
+import tensorflow as tf
+import ddsp
+import ddsp.training
+import os
+
+# Load DDSP model
+model_dir = 'model'
+ckpt_files = [f for f in tf.io.gfile.listdir(model_dir) if 'ckpt' in f]
+ckpt_name = ckpt_files[0].split('.')[0]
+ckpt = os.path.join(model_dir, ckpt_name)
+model = ddsp.training.models.Autoencoder()
+model.restore(ckpt)
 
 pTime = 0
 cTime = 0
@@ -10,7 +22,7 @@ cap = cv2.VideoCapture(0)
 success, img = cap.read()
 h, w, c = img.shape
 detector = hd.HandDetector(window_shape=(h, w))
-tm = theremin.Theremin(detector=detector)
+tm = theremin.Theremin(detector=detector, model=model)
 
 with sd.OutputStream(blocksize=tm.blocksize,
                      channels=tm.channels,
@@ -22,8 +34,6 @@ with sd.OutputStream(blocksize=tm.blocksize,
         h, w, c = img.shape
 
         detector.find_hands(img, draw=True)
-        #detector.update_center(img, 0)
-        #detector.update_center(img, 1)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
